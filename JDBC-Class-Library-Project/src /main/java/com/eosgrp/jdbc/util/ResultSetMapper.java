e11f71295412dbf6aee6c8b3b6e93304b996aa08
Ws778899 <wangshuai5154@gmail.com>
2026-04-13 23:19:04 -0400

    Update ResultSetMapper.java

--- JDBC-Class-Library-Project/src /main/java/com/eosgrp/jdbc/util/ResultSetMapper.java
+++ JDBC-Class-Library-Project/src /main/java/com/eosgrp/jdbc/util/ResultSetMapper.java
@@ -1,24 +1,9 @@
 package com.eosgrp.jdbc.util;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class JdbcExceptionHandler {
-
-    public static void handle(SQLException e) {
-        System.err.println("JDBC Error Message: " + e.getMessage());
-        System.err.println("SQL State: " + e.getSQLState());
-        System.err.println("Error Code: " + e.getErrorCode());
-
-        Throwable cause = e.getCause();
-        if (cause != null) {
-            System.err.println("Cause: " + cause.getMessage());
-        }
-
-        e.printStackTrace();
-    }
-
-    public static RuntimeException wrap(SQLException e) {
-        handle(e);
-        return new RuntimeException("Database operation failed.", e);
-    }
+@FunctionalInterface
+public interface ResultSetMapper<T> {
+    T map(ResultSet rs) throws SQLException;
 }
