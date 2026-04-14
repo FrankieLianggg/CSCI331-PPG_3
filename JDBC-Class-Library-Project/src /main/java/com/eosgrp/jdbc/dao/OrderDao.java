a5eb51a95d203a1d5d262b2f6a34b8d664ed9d14
Ws778899 <wangshuai5154@gmail.com>
2026-04-13 23:14:07 -0400

    Add files via upload

---
+++ JDBC-Class-Library-Project/src /main/java/com/eosgrp/jdbc/dao/OrderDao.java
@@ -0,0 +1,24 @@
+package com.eosgrp.jdbc.dao;
+
+import com.eosgrp.jdbc.core.QueryExecutor;
+
+import java.util.List;
+import java.util.Map;
+
+public class OrderDao {
+
+    public List<Map<String, Object>> getAllOrders() {
+        String sql = "SELECT TOP 10 * FROM Sales.[Order]";
+        return QueryExecutor.executeQuery(sql);
+    }
+
+    public List<Map<String, Object>> getOrdersByCustomerId(int customerId) {
+        String sql = "SELECT * FROM Sales.[Order] WHERE CustomerId = " + customerId;
+        return QueryExecutor.executeQuery(sql);
+    }
+
+    public List<Map<String, Object>> getRecentOrders() {
+        String sql = "SELECT TOP 10 * FROM Sales.[Order] ORDER BY OrderDate DESC";
+        return QueryExecutor.executeQuery(sql);
+    }
+}
